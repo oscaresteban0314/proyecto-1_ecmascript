cd14b815010e0e5e4a5afac25759af3a548fafaa
oscaresteban0314 <oatenas78@gmail.com>
2023-01-20 17:03:31 -0500

    clase 4 arrays y objetos

---
+++ src/es6/05-rest-spread.js
@@ -0,0 +1,8 @@
+/* arrays destructure */
+let fruits=['apple', 'banana', 'peach',];
+let[a,b,c]=fruits;
+console.log(a, b,c);
+/* objets destructure */
+let user= { username: 'oscar', age: '19' };
+let{ username, age } = user;
+console.log(username,age);
