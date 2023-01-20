2b5a9b89a503308db79ad6dcb302b5df56b59f1e
oscaresteban0314 <oatenas78@gmail.com>
2023-01-20 00:27:32 -0500

    clase 2

---
+++ src/es6/03-strings.js
@@ -0,0 +1,13 @@
+let hello = 'Hello';
+let world = 'World';
+let helloworld = hello+' '+world+'!';
+
+//template literals
+let helloworld2 = `${hello} ${world}!`;
+console.log(helloworld2);
+//multi-line string
+let lorem=`
+Esto es un string
+esto es otra linea
+`;
+console.log(lorem);
