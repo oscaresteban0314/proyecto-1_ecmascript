8dc4b916eea15b5598c28842121c7a6cf1672f86
oscaresteban0314 <oatenas78@gmail.com>
2023-01-22 07:34:31 -0500

    spread op

--- src/es6/05-rest-spread.js
+++ src/es6/05-rest-spread.js
@@ -6,3 +6,16 @@ console.log(a, b,c);
 let user= { username: 'oscar', age: '19' };
 let{ username, age } = user;
 console.log(username,age);
+
+//spread operator
+let person ={name: 'oscare', age: 19};
+let country ='Co';
+let data={...person, country};
+console.log(data);
+//rest
+function sum(num1, ...values){
+    console.log(values);
+    console.log(num1+values[5]);
+    return num1+values[5];
+}
+sum(1,2,3,4,5,6,7,8);
